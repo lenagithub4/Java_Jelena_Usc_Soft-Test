0c6b22c581b840b5261f5820eafeb8c2c6c128af
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-01-30 22:27:32 +0200

    добавлено несколько функций

--- sandbox/src/main/java/myFirstProgram.java
+++ sandbox/src/main/java/myFirstProgram.java
@@ -2,9 +2,36 @@ public class MyFirstProgram {
 	
 	
 	public static void main (String[] args) {
-		System.out.println("Hello, my World!");
-		
+		hello("world");
+		hello("Admin");
+
+		double l = 3.5;
+		System.out.println("Площадь квадрата со стороной  " + l + " = " + square(l));
+
 	}
+		public static void hello(String slovo1) {
+
+			System.out.println("Hello, " + slovo1  + "!");
+		}
+
+		public static void match() {
+			System.out.println(2 + 2);
+			System.out.println(2 / 2);
+			System.out.println(1.0 / 2);
+			System.out.println(1 / 2.0);
+			System.out.println("2" + "A");
+			System.out.println("2" + 2);
+			System.out.println(2 + 2 * 2);
+			System.out.println("2 + 2 =" + (2 + 2));
+		}
+
+		public static double square(double len) {
+			return (len * 2);
+		}
+
+
+
+
 	
 	
 }

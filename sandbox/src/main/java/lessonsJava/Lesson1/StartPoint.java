142d11e52f8602e3690d53e4ac7c815d06cac8c3
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-02-01 22:03:09 +0200

    Add from IDEA

---
+++ sandbox/src/main/java/lessonsJava/Lesson1/StartPoint.java
@@ -0,0 +1,14 @@
+package lessonsJava.Lesson1;
+
+public class StartPoint {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(2,5);
+        Point p2 = new Point(7,2);
+
+        System.out.println(" 1st point coordinates");
+        p1.Print();
+        System.out.println(" 2nd point coordinates");
+        p2.Print();
+    }
+}

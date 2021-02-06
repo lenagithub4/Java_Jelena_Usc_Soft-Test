b849efa239da464363e4de5ab1ce86579c29ff2f
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-02-06 22:57:28 +0200

    Add from IDEA Point1 and StartPoint1 are added

--- sandbox/src/test/java/LessonsJava/Lesson1/PointTests.java
+++ sandbox/src/test/java/LessonsJava/Lesson1/PointTests.java
@@ -1,14 +1,14 @@
 package LessonsJava.Lesson1;
 
-import lessonsJava.Lesson1.Point1;
+import lessonsJava.Lesson1.Point;
 import org.testng.Assert;
 import org.junit.Test;
 
-public class Point1Tests {
+public class PointTests {
     @Test
     public void testDistance() {
-     Point1 p1 = new Point1(0,5);
-     Point1 p2 = new Point1(0,0);
+     Point p1 = new Point(0,5);
+     Point p2 = new Point(0,0);
 
         Assert.assertEquals( p1.distance(p2), 5.0);
     }
@@ -16,22 +16,22 @@ public class Point1Tests {
 
     @Test
     public void testDistance1() {
-        Point1 p1 = new Point1(0,5);
-        Point1 p2 = new Point1(0,0);
+        Point p1 = new Point(0,5);
+        Point p2 = new Point(0,0);
 
         Assert.assertEquals( p1.distance(p2), 1.0);
     }
     @Test
     public void testDistance2() {
-        Point1 p1 = new Point1(1,1);
-        Point1 p2 = new Point1(1,2);
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(1,2);
 
         Assert.assertEquals( p1.distance(p2), 1.0);
     }
     @Test
     public void testDistance3() {
-        Point1 p1 = new Point1(1,1);
-        Point1 p2 = new Point1(1,2);
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(1,2);
 
         Assert.assertEquals( p1.distance(p2), -1.0);
     }

ffe898f0521e26f3a4d340c3915495ee36f0cf79
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-02-20 23:58:53 +0200

    Lekcija 2.8 HelperBase+ContactHelper

--- addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class HelperBase {
     protected WebDriver wd;
@@ -38,5 +39,9 @@ public class HelperBase {
         } catch (NoAlertPresentException e) {
             return false;
         }
+    }
+
+    protected void select(By locator, String data) {
+      new Select(wd.findElement(locator)).selectByVisibleText(data);
     }
 }

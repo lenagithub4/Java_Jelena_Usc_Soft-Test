eda1227c940802588aba61bbe3c433f064f78e4c
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-02-24 18:27:59 +0200

    Lekcija 2.9 GroupModificationTests

--- addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -44,6 +44,7 @@ public class GroupHelper extends HelperBase {
         click(By.name("selected[]"));
     }
 
+
     public void initGroupModification() {
         click(By.name("edit"));
     }
--- addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,10 +4,11 @@ import org.junit.Test;
 import ru.stqa.pft.addressbook.model.GroupForm;
 
 public class GroupModificationTests extends TestBase {
+
     @Test
     public void testGroupModification() {
         app.getGroupHelper().gotoGroupPage();
-        app.getGroupHelper().select();
+        app.getGroupHelper().SelectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupForm("test1", "test2", "test3"));
         app.getGroupHelper().submitGroupModification();

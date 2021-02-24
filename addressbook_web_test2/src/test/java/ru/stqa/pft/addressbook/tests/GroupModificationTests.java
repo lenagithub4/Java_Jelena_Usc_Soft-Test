a6b0fbf68787454885b02b19e5486a67d3bb0c20
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-02-24 18:09:45 +0200

    Lekcija 2.9 GroupModificationTests

---
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.junit.Test;
+import ru.stqa.pft.addressbook.model.GroupForm;
+
+public class GroupModificationTests extends TestBase {
+    @Test
+    public void testGroupModification() {
+        app.getGroupHelper().gotoGroupPage();
+        app.getGroupHelper().select();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupForm("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnGroupPage();
+
+    }
+}
+

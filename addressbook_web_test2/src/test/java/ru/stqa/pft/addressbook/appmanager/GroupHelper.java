593f22dbd16de0226cb7d8e88e79b6309d7440ef
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-02-25 23:54:33 +0200

    Lekcija 2.9 GroupModificationTests

--- addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -45,11 +45,7 @@ public class GroupHelper extends HelperBase {
     }
 
 
-    public void initGroupModification() {
-        click(By.name("edit"));
-    }
+    public void initGroupModification() { click(By.name("edit"));  }
 
-    public void submitGroupModification() {
-        click(By.name("update"));
-    }
+    public void submitGroupModification() {  click(By.name("update"));  }
 }

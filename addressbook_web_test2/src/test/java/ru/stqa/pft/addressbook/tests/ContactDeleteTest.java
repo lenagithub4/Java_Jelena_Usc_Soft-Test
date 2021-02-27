cc4a006e6fd55ef7a9a81904892d865b3105e264
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-02-28 00:27:51 +0200

    Lekcija 2.9 GroupModificationTests, GroupHELPER , ContractDeleteTest

---
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTest.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class ContactDeleteTest extends TestBase {
+    @Test
+    public void testContactDelete() {
+        app.getNavigationHelper().GoToHome();
+        app.getContactHelper().selectContact();
+        app.getContactHelper().selectContactEdit();
+        app.getGroupHelper().deleteContact();
+    }
+}

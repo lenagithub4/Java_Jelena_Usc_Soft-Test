7aadba5a1a74956ccd5fe1ffff04ae5dfcb36e63
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-03-04 23:43:22 +0200

    Lekcija 2.9 FIreFox and ChromeDrivers

--- addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,20 +2,34 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    private WebDriver wd;
+    WebDriver wd;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private ContactData.NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
 
-        wd = new ChromeDriver();
+        if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.EDGE) {
+            wd = new EdgeDriver();
+        }
         WebDriver.Timeouts implicitlyWait = wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
        wd.get("http://localhost/addressbook/");

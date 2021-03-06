86bc40c934fe0981ecd8c263aad18dc3b9eed7f5
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-03-06 08:08:54 +0200

    Equality. Lekcija 3.4

---
+++ sandbox/src/main/java/lessonsJava/lesson3/Equality.java
@@ -0,0 +1,17 @@
+package lessonsJava.lesson3;
+
+public class Equality {
+
+    public static void main(String[] args) {
+
+        String s1 = "firefox";
+        String s2 = new String(s1);
+        String s3 = "fire"+"fox";
+
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1==s3);
+        System.out.println(s1.equals(s3));
+
+    }
+}

c89743395b7e818cbe8069188cdb4c33ff0cd5d6
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-03-20 18:45:00 +0200

    Lekcija 4.7

--- addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupForm;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -21,15 +22,20 @@ public class GroupModificationTests extends TestBase {
         // before = app.getGroupHelper().getGroupCount(); //check count of groups before modif
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupForm("test1", "test2 modif", "test3 modif"));
+        GroupForm group = new GroupForm(before.get(before.size()-1).getId(),"test1", "test2 modif", "test3 modif"); // Id formula
+        app.getGroupHelper().fillGroupForm(group);
 
         app.getGroupHelper().submitGroupModification();
         app.getNavigationHelper().gotoGroupPage();
         //int after = app.getGroupHelper().getGroupCount(); //check count of groups after modif
         List<GroupForm> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals( after.size(), before.size());
+        Assert.assertEquals(after.size(), before.size());
 
+        //Lection 4.7. non-sorted collections
+        before.remove(before.size()-1);
+        before.add(group);
 
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object> (after)); // Variant 2 lists comparition
     }
 }
 

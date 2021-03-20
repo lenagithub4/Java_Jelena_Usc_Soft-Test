f14127d62e5ddc992fa58f18d2e749513980f4d3
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-03-20 19:36:40 +0200

    Lekcija 4.9

--- addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -84,7 +84,7 @@ public class GroupHelper extends HelperBase{
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {
             String name = element.getText();
-            String id=element.findElement(By.tagName("input")).getAttribute("value");
+            int id= Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
             GroupForm group = new GroupForm(id, name, null, null);
             groups.add(group);
         }
--- addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreateTest.java
@@ -9,7 +9,6 @@ import java.util.HashSet;
 import java.util.List;
 
 
-
 public class GroupCreateTest extends TestBase {
 
 
@@ -19,25 +18,28 @@ public class GroupCreateTest extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupForm> before = app.getGroupHelper().getGroupList();
         //int before = app.getGroupHelper().getGroupCount(); //check count of groups before addition
-        GroupForm group = new GroupForm("test1", null, null);
+        GroupForm group = new GroupForm("test2", null, null);
         app.getGroupHelper().createGroup(group);
         app.getNavigationHelper().gotoGroupPage();
         List<GroupForm> after = app.getGroupHelper().getGroupList();
-       // int after = app.getGroupHelper().getGroupCount(); //check count of groups after addition
-        Assert.assertEquals( after.size(), before.size() + 1);
+        // int after = app.getGroupHelper().getGroupCount(); //check count of groups after addition
+        Assert.assertEquals(after.size(), before.size() + 1);
+
+/* old max function */
+        //int max = 0;
+        //for (GroupForm q : after) {
+        //    if (q.getId() > max) {
+        //        max = q.getId();
+         //   }
+        //}
 
 
+       // Comparator<? super GroupForm> byId = (Comparator<GroupForm>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId()); //Lekcija 4.9
+
+        //int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId()); // Lekcija 4.8
         before.add(group);
-int max = 0;
-for (GroupForm q : after) {
-    if (q.getId()>max) {
-    max = q.getId();
-    }
-}
-
-        group.setId(max); // Lekcija 4.8
-         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object> (after)); // Variant 2 lists comparition
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); // Variant 2 lists comparition
 
         //app.getGroupHelper().initGroup();
         //app.getGroupHelper().fillGroupForm(new GroupForm("test1", null, null));

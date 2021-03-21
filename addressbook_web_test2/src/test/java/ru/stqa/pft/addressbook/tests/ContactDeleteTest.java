eff8b2d55df9ea9632c3ea312541bc616c41632d
Jelena Uscelemova <jelena.uscelemova@aaro.se>
2021-03-21 21:16:19 +0200

    Lekcija 4.6 Contacts Collections comparition

--- addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTest.java
+++ addressbook_web_test2/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeleteTest.java
@@ -14,19 +14,26 @@ public class ContactDeleteTest extends TestBase {
         List<ContactData> before = app.getContactHelper().getContactList();
         //int before = app.getContactHelper().getContactCount();
 
-        if (! app.getContactHelper().isThereAContact()) {
+        if (!app.getContactHelper().isThereAContact()) {
             if (!app.getGroupHelper().isThereAGroup()) {
                 app.getGroupHelper().createGroup(new GroupForm("test1", null, null));
             }
-            app.getContactHelper().createContact(new ContactData("Name", "Name2", "Surname",   "test1"), true);
+            app.getContactHelper().createContact(new ContactData("Name", "Name2", "Surname", "test1"), true);
         }
         app.getNavigationHelper().GoToHome();
-        app.getContactHelper().selectContact(before.size()-1);
-        app.getContactHelper().selectContactEdit(before.size()-1);
+        app.getContactHelper().selectContact(before.size() - 1);
+        app.getContactHelper().selectContactEdit(before.size() - 1);
         app.getContactHelper().deleteContact();
         app.getNavigationHelper().GoToHome();
         List<ContactData> after = app.getContactHelper().getContactList();
-       // int after = app.getContactHelper().getContactCount();
+        // int after = app.getContactHelper().getContactCount();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        // lesson 4.7
+        before.remove(before.size() - 1); // back before list to preview state (for Variant 1, Variant 2)
+        //for (int i = 0; i < after.size(); i++) { // no necessary Variant 1
+        //    Assert.assertEquals(before.get(i), after.get(i));// no necessary Variant 1
+        Assert.assertEquals(before, after);
+        }
     }
-}
+
